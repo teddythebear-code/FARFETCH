78658add08a1e092e0c8244e7559666f96753ff7
nosaozemoyaefeanthony <nosaozemoyaefeanthony@gmail.com>
2025-09-02 05:47:32 -0700

    done with Find Out Move
    done with Find_Out_Move.jsx and Find_Out_Move.css. Note this is the old value for Find_Out_Move.jsx.

--- Frontend/src/Components/Find Out More/Find_Out_Move.jsx
+++ Frontend/src/Components/Find Out More/Find_Out_Move.jsx
@@ -1,9 +1,19 @@
 import './Find_Out_Move.css';
-
-const Find_Out_Move = () => {
+import image from "../../Assets/Image/1a32a6bf83c17b862cbbfba9fd776f3c.jpg"
+const Find_Out_Move = (props) => {
+  
   return (
-    <div>
-      
+    <div className='Find_Out_Move'>
+      <div className='Find_Out_Move_info'>
+        <h2>The best of the new season</h2>
+        <p>Discover fall's key pieces from Prada, Miu Miu, Jil Sander and more</p>
+        <div className='Find_Out_Move_but'>
+          <button>Find Out Move</button>
+        </div>
+          </div>
+      <div className='Find_Out_Move_image'>
+        <img src={image} alt="" />
+      </div>
     </div>
   )
 }
