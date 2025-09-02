7ee0c1ab75dc08a4f5bc5a89df1a4e78e071e716
nosaozemoyaefeanthony <nosaozemoyaefeanthony@gmail.com>
2025-09-02 04:37:40 -0700

    create Find_Out_Move.jsx
    Creare Find_Out_Move.Jsx and Find_Out_Move.CSS.Note: added the import (Global styles, Utils styles, Variables styles) to the Find_Out_Move.CSS document. Also export as default

---
+++ Frontend/src/Components/Find Out More/Find_Out_Move.jsx
@@ -0,0 +1,11 @@
+import './Find_Out_Move.css';
+
+const Find_Out_Move = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Find_Out_Move
--- Frontend/src/Page/Home/Home.jsx
+++ Frontend/src/Page/Home/Home.jsx
@@ -1,10 +1,12 @@
 import "./Home.css";
 
+import Find_Out_Move from "../../Components/Find Out More/Find_Out_Move";
 const Home = () => {
 
 
   return (
     <div>
+      <Find_Out_Move/>
     </div>
   )
 }
