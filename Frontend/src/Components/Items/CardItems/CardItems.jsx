f9221eb0d335eeb14d8bcaefc9429602984aeda7
nosaozemoyaefeanthony <nosaozemoyaefeanthony@gmail.com>
2025-09-03 14:42:17 +0100

    Added Props and link to the Items and Card Items
    Added the props value and the link needed for the Components

--- Frontend/src/Components/Items/CardItems/CardItems.jsx
+++ Frontend/src/Components/Items/CardItems/CardItems.jsx
@@ -1,5 +1,4 @@
 import "./CardItems.css";
-import image from "../../../Assets/Image/1a32a6bf83c17b862cbbfba9fd776f3c.jpg";
 import {Link} from "react-router-dom"
 
 import svgIcon1 from "../../../Assets/Svg/favoritesvg/favorite-svgrepo-com.svg";
@@ -11,43 +10,49 @@ const CardItems = (props) => {
   const [hovered, setHovered] = useState(false);
 
   return (
-    <div className="CardItems">
-      <div className="CardItemsImage">
-        <img src={image} alt="" width={20} />
+    <div className="CardItems"> 
+      <Link to={props.CardItemLink}>
+          <div className="CardItemsImage">
+            <img src={props.image} alt={props.image} />
 
-        <p className="Extra">
-          <span className="ExtraSpan">Extra 20% off</span>
-        </p>
+            <p className="Extra">
+              
+              {props.Extra}
 
-        <div
-          className="svgIcon"
-          onMouseEnter={() => setHovered(true)}
-          onMouseLeave={() => setHovered(false)}
-        >
-          <div className={hovered ? "onMouseLeavesvgIcon1" : "svgIcon1"}>
-            <img src={svgIcon1} alt="icon1" />
+              {/*! Note: add the value to the (<span className="ExtraSpan">Extra 20% off</span>) Extra props if you want to use it*/}
+
+            </p>
+
+            <div
+              className="svgIcon"
+              onMouseEnter={() => setHovered(true)}
+              onMouseLeave={() => setHovered(false)}
+            >
+              <div className={hovered ? "onMouseLeavesvgIcon1" : "svgIcon1"}>
+                <img src={svgIcon1} alt="icon1" />
+              </div>
+              <div className={hovered ? "onMouseLeavesvgIcon2" : "svgIcon2"}>
+                <img src={svgIcon2} alt="icon2" />
+              </div>
+            </div>
           </div>
-          <div className={hovered ? "onMouseLeavesvgIcon2" : "svgIcon2"}>
-            <img src={svgIcon2} alt="icon2" />
+
+          <div className="CardItemsInfo">
+            <div className="CardItemsInfoName">
+              <h3>{props.CardItemsInfoName1}</h3>
+              <p>{props.CardItemsInfoName2}</p>
+            </div>
+
+            <div className="CardItemsInfoPrice">
+              <p className="CardItemsInfoPriceOldprice">{props.CardItemsInfoPriceOldprice}</p>
+              <p>{props.CardItemsInfoPrice1}</p>
+              <p>{props.CardItemsInfoPrice2}</p>
+              <p>
+                <span className="spanprice">{props.CardItemsInfoPrice3}</span>
+              </p>
+            </div>
           </div>
-        </div>
-      </div>
-
-      <div className="CardItemsInfo">
-        <div className="CardItemsInfoName">
-          <h3>savette</h3>
-          <p>small tondo shoulder bag</p>
-        </div>
-
-        <div className="CardItemsInfoPrice">
-          <p className="CardItemsInfoPriceOldprice">$213</p>
-          <p>-20%</p>
-          <p>$244</p>
-          <p>
-            <span className="spanprice">$2,240</span>
-          </p>
-        </div>
-      </div>
+        </Link>
     </div>
   );
 };
--- Frontend/src/Components/Items/Items.jsx
+++ Frontend/src/Components/Items/Items.jsx
@@ -1,17 +1,19 @@
 import './Items.css'
 
+import { Link } from 'react-router-dom'
 import CardItems from './CardItems/CardItems'
 
-const Items = () => {
+const Items = (props) => {
   return (
     <div className='Items' >
       <div className='ItemsTop'>
 
         <div className='ItemsTopText'>
-          <h2>Enjoy 20% off selected styles</h2>
+          <h2>{props.ItemsTopText}</h2>
         </div>
-        <div >
-          <button className='ItemsTopButton'>Shon Now</button>
+        <div ><Link to={props.ItemsTopText.Link} >
+        <button className='ItemsTopButton'>Shon Now</button>
+        </Link>
         </div>
       </div>
       <div className='ItemsCard'>
